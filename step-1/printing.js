27bd7a0108e0ccd473e939299f036fccc670a72c
amitkc.gupta <amitkc.gupta@infobeans.com>
2025-11-02 00:35:53 +0530

    step-1 printing message in console with different methods.

---
+++ step-1/printing.js
@@ -0,0 +1,8 @@
+console.log("amit");
+
+// process.stdout.write("Hi there!\n");
+
+console.warn("This is a warning message.");
+console.error("This is an error message.");
+
+console.table({ name: "Alice", age: 30 });
