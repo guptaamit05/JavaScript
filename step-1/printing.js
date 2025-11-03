9666c067acfecc7355791b1a1adab1daf18a6595
guptaamit05 <gupta.05.amit@gmail.com>
2025-11-03 11:33:54 +0530

    adding time log.

--- step-1/printing.js
+++ step-1/printing.js
@@ -6,3 +6,9 @@ console.warn("This is a warning message.");
 console.error("This is an error message.");
 
 console.table({ name: "Alice", age: 30 });
+
+console.time("loopTime");
+for (let i = 0; i < 100000; i++) {
+  // Simulating a time-consuming task
+}
+console.timeEnd("loopTime");
