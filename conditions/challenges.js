b70e4d5564a686194175d8b1bc607dcbf0803c29
guptaamit05 <gupta.05.amit@gmail.com>
2025-11-03 21:58:52 +0530

    conditions if else in JS.

---
+++ conditions/challenges.js
@@ -0,0 +1,39 @@
+// Check if a number is greater than another number or not
+// let a = 10;
+// let b = 20;
+// if( a > b )
+//     console.log("A is Greater than B")
+// else
+//     console.log("B is greater thatn A...")
+
+// console.log("This is regular expression...")
+
+
+
+// Check if a variable is number or not...
+// let score  = 534
+// if(typeof score == "number"){
+//     console.log("It is number")
+// }else{
+//     console.log("Not a number")
+// }
+
+
+// Check if a value is true || false 
+// let c = "true"
+// if(c){
+//     console.log("Okay")
+// }else{
+//     console.log("Not Okay....")
+// }
+
+
+// Checking if array is empty or not...
+// let items = [1,2,3,4,4,5]
+// let it = []
+// if(it.length===0){
+// console.log("array has data...")
+// }else{
+//     console.log("array is empty")
+// }
+
